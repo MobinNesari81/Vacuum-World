20e0eef960ecb6557adcdeb133a24bd40f1d58a7
Mobin Nesari <mobinnesari81@gmail.com>
2023-10-17 11:40:57 +0330

    GG EZ

--- environment.py
+++ environment.py
@@ -4,7 +4,7 @@ import pygame
 import random
 
 class VacuumWorldEnv(gym.Env):
-    def __init__(self, n_rows:int = 5, n_cols:int = 5, max_timestep=100) -> None:
+    def __init__(self, n_rows:int = 5, n_cols:int = 5, max_timestep=100, dirt_chance:float = 0.4, is_obstacle:bool = False, obstacle_chance: float = 0.3, dirt_uncertainnity: bool = False, movement_uncertainity: bool = False, suck_uncertainity: bool = False, observation_uncertainity: bool = False) -> None:
         self.n_rows = n_rows
         self.n_cols = n_cols
         self.MAX_STEP = max_timestep
@@ -23,23 +23,30 @@ class VacuumWorldEnv(gym.Env):
         self.pos_x = 0
         self.pos_y = 0
         self.time_step = 1
+        self.dirt_chance = dirt_chance
+        self.is_obstacle = is_obstacle
+        self.obstacle_chance = obstacle_chance
+        self.dirt_uncertainity = dirt_uncertainnity
+        self.movement_uncertainity = movement_uncertainity
+        self.suck_uncertainity = suck_uncertainity
+        self.observation_uncertainity = observation_uncertainity
 
-        self._generate_map()
+        self._generate_map(dirt_chance=self.dirt_chance, is_obstacle=False, obstacle_chance=obstacle_chance)
                 
 
-    def _generate_map(self, is_obstacle=False):
+    def _generate_map(self, dirt_chance: float = 0.5, is_obstacle=False, obstacle_chance: float = None):
         for i in range(self.n_rows):
             blocks = 0
             for j in range(self.n_cols):
                 epsilon = random.random()
                 if is_obstacle:
-                    if epsilon > 2/3:
+                    if epsilon > 1-dirt_chance:
                         self.map[i][j] = 1
-                    elif epsilon < 1/3 and blocks < self.n_cols:
+                    elif epsilon < obstacle_chance and blocks < self.n_cols:
                         self.map[i][j] = -1
-                        blocks += 1 
+                        blocks += 1
                 else:
-                    if epsilon > 0.5:
+                    if epsilon > 1-dirt_chance:
                         self.map[i][j] = 1
 
     def _get_obs(self) -> tuple:
@@ -47,6 +54,14 @@ class VacuumWorldEnv(gym.Env):
 
     def _get_info(self) -> dict:
         return {'time_step': self.time_step}
+    
+    def _dirt_process(self) -> None:
+        for i in range(self.n_rows):
+            for j in range(self.n_cols):
+                if self.map[i][j] != -1:
+                    epsilon = random.random()
+                    if epsilon > 1 - self.dirt_chance:
+                        self.map[i][j] = 1
     
     def reset(self):
         super().reset()
@@ -79,15 +94,42 @@ class VacuumWorldEnv(gym.Env):
             reward = -1
         
         if not (tmp_pos[0] < 0 or tmp_pos[0] >= self.n_rows or tmp_pos[1] < 0 or tmp_pos[1] >= self.n_cols or self.map[tmp_pos[0]][tmp_pos[1]] == -1):
-            self.pos_x = tmp_pos[0]
-            self.pos_y = tmp_pos[1]
+            if self.movement_uncertainity:
+                epsilon = random.random()
+                if epsilon > 0.2:
+                    self.pos_x = tmp_pos[0]
+                    self.pos_y = tmp_pos[1]
+                else:
+                    pass
         
         if action == 5:
-            if self.map[self.pos_x][self.pos_y] == 1:
-                reward = 10
-                self.map[self.pos_x][self.pos_y] = 0
+            if self.suck_uncertainity:
+                epsilon = random.random()
+                if epsilon > 0.25:
+                    if self.map[self.pos_x][self.pos_y] == 1:
+                        reward = 10
+                        self.map[self.pos_x][self.pos_y] = 0
+                else:
+                    reward = -1
+                    self.map[self.pos_x][self.pos_y] = 1
+            else:
+                if self.map[self.pos_x][self.pos_y] == 1:
+                        reward = 10
+                        self.map[self.pos_x][self.pos_y] = 0
+            
         
-        return self._get_obs(), reward, False, self._get_info()
+        if self.dirt_uncertainity:
+            self._dirt_process()
+        
+        obs = self._get_obs()
+        if self.observation_uncertainity:
+            epsilon = random.random()
+            if epsilon > 0.1:
+                pass
+            else:
+                obs = (obs[0], obs[1], 1-obs[2])
+                
+        return obs, reward, False, self._get_info()
 
 
 

004f5d381cfff79223ae159c57e1f8dedd2ba0bc
Mobin Nesari <mobinnesari81@gmail.com>
2023-10-19 23:04:20 +0330

    Implementing TraGent

--- agents.py
+++ agents.py
@@ -96,3 +96,112 @@ class no_location_agent:
 
 
             return max_idx + 1     
+
+
+class TraGent:
+    # Trace Agent implementation
+    def __init__(self) -> None:
+        self.memory_stack = []
+        self.destination = None
+        self.stal_flag = False
+        self.prev_action = None
+        self.last_row_direction = None
+
+    def act(self, observation: tuple, terminated: bool):
+        if terminated:
+            return 0
+        
+        if observation[2] == 1:
+            if self.destination:
+                if self.destination[-1] == (observation[0], observation[1]):
+                    self.destination.pop()
+            return 5
+        
+        else:
+            if  not self.destination:
+                # Trace start to last
+                if not self.prev_action:
+                    # Initial State
+                    self.memory_stack.append((observation[0], observation[1]))
+                    self.prev_action = 'right'
+                    return 1
+                
+                else:
+                    if self.prev_action == 'right':
+                        if self.memory_stack[-1] == (observation[0], observation[1]):
+                            if self.stal_flag:
+                                self.stal_flag = False
+                                self.prev_action = 'down'
+                                self.last_row_direction = 'right'
+                                return 4
+                            else:
+                                self.stal_flag = True
+                                return 1
+                        
+                        else:
+                            self.memory_stack.append((observation[0], observation[1]))
+                            self.stal_flag = False
+                            return 1
+                    
+                    if self.prev_action == 'left':
+                        if self.memory_stack[-1] == (observation[0], observation[1]):
+                            if self.stal_flag:
+                                self.stal_flag = False
+                                self.prev_action = 'down'
+                                self.last_row_direction = 'left'
+                                return 4
+                            else:
+                                self.stal_flag = True
+                                return 2
+                        else:
+                            self.memory_stack.append((observation[0], observation[1]))
+                            self.stal_flag = False
+                            return 2
+                    
+                    if self.prev_action == 'down':
+                        if self.memory_stack[-1] == (observation[0], observation[1]):
+                            if self.stal_flag:
+                                self.destination = self.memory_stack[::-1]
+                                self.memory_stack = []
+                                self.stal_flag = False
+                                self.prev_action = False
+                                self.last_row_direction = None
+                                return 2
+                            
+                            else:
+                                self.stal_flag = True
+                                return 4
+                        
+                        else:
+                            if self.last_row_direction == 'right':
+                                self.prev_action = 'left'
+                                self.memory_stack.append((observation[0], observation[1]))
+                                return 2
+                            else:
+                                self.prev_action = 'right'
+                                self.memory_stack.append((observation[0], observation[1]))
+                                return 1
+
+                
+                
+            
+            else:
+                # Trace last to start
+                if self.destination[0] == (observation[0], observation[1]):
+                    self.destination.pop(0)
+                delta_x = observation[0] - self.destination[0][0]
+                delta_y = observation[1] - self.destination[0][1]
+                if len(self.destination) == 1:
+                    self.destination = None
+                if delta_x > 0:
+                    print('up')
+                    return 3
+                elif delta_x < 0:
+                    print('down')
+                    return 4
+                elif delta_y > 0:
+                    print('left')
+                    return 2
+                elif delta_y < 0:
+                    print('right')
+                    return 1
--- test.py
+++ test.py
@@ -1,15 +1,22 @@
 import gym
 from environment import VacuumWorldEnv
+from agents import TraGent
 
 gym.register(
     id='VacuumWorld-v0',
     entry_point='environment:VacuumWorldEnv',
     kwargs={'n_cols': 5, 'n_rows': 5, 'max_timestep': None}
 )
-env = gym.make('VacuumWorld-v0', n_cols=2, n_rows=1, max_timestep=10)
-env.reset()
-
-for i in range(100):
+env = gym.make('VacuumWorld-v0', n_cols=2, n_rows=2, max_timestep=30)
+obs, info = env.reset()
+tragent = TraGent()
+for i in range(30):
     print(f"--- Step: {i} ---")
-    action = env.action_space.sample()
-    print(env.step(action))
+    print(obs)
+    action = tragent.act(obs, False)
+    print(f'action: {action}')
+    print(f'memory_stack: {tragent.memory_stack}')
+    print(f'destination: {tragent.destination}')
+    print(f'prev_action:{tragent.prev_action}')
+    print(tragent.stal_flag)
+    obs, reward, term, info = env.step(action)
